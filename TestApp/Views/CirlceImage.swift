1abc769e349fb6a05e0b2fbb17513c07eae309d0
himoji <ma.lyaka@bk.ru>
2024-05-02 23:44:10 +0500

    + Image carousel + fixed navigation

--- TestApp/Views/CirlceImage.swift
+++ TestApp/Views/CirlceImage.swift
@@ -1,18 +1,62 @@
 import SwiftUI
 
-struct CircleImage: View {
-    var image: Image
+struct ImageCarousel: View {
+    var images: Array<String>
+    var body: some View {
+        AutoScroller(imageNames: images)
+    }
+}
+struct AutoScroller: View {
+    var imageNames: [String]
+    let timer = Timer.publish(every: 3.0, on: .main, in: .common).autoconnect()
+    @State private var selectedImageIndex: Int = 0
 
     var body: some View {
-        image
-            .clipShape(Circle())
-            .overlay {
-                Circle().stroke(.white, lineWidth: 4)
+        ZStack {
+           
+            Color.secondary
+                .ignoresSafeArea()
+
+         
+            TabView(selection: $selectedImageIndex) {
+              
+                ForEach(0..<imageNames.count, id: \.self) { index in
+                    ZStack(alignment: .topLeading) {
+                   
+                        Image("\(imageNames[index])")
+                            .resizable()
+                            .tag(index)
+                    }
+                    .background(.thinMaterial)
+                }
             }
-            .shadow(radius: 7)
+            
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .ignoresSafeArea()
+
+            HStack {
+                ForEach(0..<imageNames.count, id: \.self) { index in
+               
+                    Capsule()
+                        .fill(Color.white.opacity(selectedImageIndex == index ? 1 : 0.33))
+                        .frame(width: 35, height: 8)
+                        .onTapGesture {
+                           
+                            selectedImageIndex = index
+                        }
+                }
+                .offset(y: 130)
+            }
+        }.frame(width:300, height: 200)
+        .onReceive(timer) { _ in
+            
+            withAnimation(.default) {
+                selectedImageIndex = (selectedImageIndex + 1) % imageNames.count
+            }
+        }
     }
 }
 
-#Preview {
-    CircleImage(image: Image("turtlerock"))
+#Preview() {
+    ImageCarousel(images: ["astana","astana","astana","astana"])
 }
--- TestApp/Views/WeatherView.swift
+++ TestApp/Views/WeatherView.swift
@@ -3,102 +3,96 @@ import SwiftUI
 
 struct WeatherView: View {
     var city: City
-    let temperature: Int = 1 // Temperature in Celsius
-    @State public var speed = 0.0
-    @State private var isEditing = false
-
-
+    @State private var temperature: Double = 0.0 // Temperature in Celsius
+    
+    
     var body: some View {
-        let gradientColors = gradientColorsForTemperature(Int(speed))
-        
         ScrollView {
             VStack(spacing: 16) {
+                // Map view
                 MapView(coordinate: city.locationCoordinate)
                     .frame(height: 200)
                     .cornerRadius(10)
                     .padding(.horizontal)
                 
-                VStack(alignment: .leading, spacing: 16) {
-                    Text("\(city.name), \(city.country)")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(.primary)
-                    
-                    Divider()
-                    
-                    HStack {
-                        Text("Temperature:")
-                            .font(.headline)
-                            .foregroundColor(.primary)
-                        Text("\(Int(speed))°C")
-                            .font(.headline)
-                            .foregroundColor(.primary)
-                    }
-                    
-                    HStack {
-                        Text("Conditions:")
-                            .font(.headline)
-                            .foregroundColor(.primary)
-                        Text("Sunny with some wind")
-                            .font(.headline)
-                            .foregroundColor(.primary)
-                    }
-                    
-                    Divider()
-                    
-                    Text("About \(city.name)")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(.primary)
-                    
-                    Text(city.description)
-                        .font(.body)
-                        .foregroundColor(.primary)
-                }
-                .padding()
-                .background(Color(.systemBackground))
-                .cornerRadius(10)
-                .padding()
+                // Weather details
+                WeatherDetailsView(city: city, temperature: $temperature)
+                    .padding(.all)
+                
+                ImageCarousel(images: ["astana","astana", "astana"])
+                    .padding(.bottom, 12.0)
+                   
             }
-            VStack {
-                    Slider(
-                        value: $speed,
-                        in: -40...40,
-                        onEditingChanged: { editing in
-                            isEditing = editing
-                        }
-                    )
-                    Text("\(speed)")
-                        .foregroundColor(isEditing ? .red : .blue)
-                }
-            .padding(.top, 16)
         }
+        .padding(.top, 10.0)
         .navigationTitle(city.name)
         .navigationBarTitleDisplayMode(.inline)
-        .background(LinearGradient(gradient: Gradient(colors: gradientColors), startPoint: .top, endPoint: .bottom)
-            )
-    }
-    
-    private func gradientColorsForTemperature(_ temperature: Int) -> [Color] {
-        if temperature <= 0 {
-            // Cold temperature, blue gradient
-            return [Color.black.opacity(0.8), Color.blue.opacity(0.8), Color.blue.opacity(1)]
-        } else if temperature > 0 && temperature <= 10{
-            // Hot temperature, orange gradient
-            return [Color.blue.opacity(0.6), Color.blue.opacity(0.4)]
-        } else if temperature > 10 && temperature <= 20{
-            // Hot temperature, orange gradient
-            return [Color.blue.opacity(0.4), Color.mint.opacity(0.6)]
-        }
-        else if temperature > 20 && temperature < 30 {
-            // Hot temperature, orange gradient
-            return [Color.mint.opacity(0.6), Color.orange.opacity(0.4)]
-        }else {
-            // Moderate temperature, green gradient
-            return [Color.orange.opacity(0.4), Color.orange.opacity(0.6)]
-        }
+        .background(
+            LinearGradient(colors: [.init(red: 48/255, green: 64/255, blue: 96/255),
+                                     .init(red: 39/255, green: 40/255, blue: 62/255)],
+                           startPoint: .bottom, endPoint: .top)
+        )
     }
 }
+
+struct WeatherDetailsView: View {
+    var city: City
+    @Binding var temperature: Double
+    
+    var body: some View {
+        ZStack {
+            VStack(alignment: .leading, spacing: 16) {
+                // City name
+                Text("\(city.name), \(city.country)")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+                
+                Divider()
+                
+                // Temperature
+                HStack {
+                    Text("Temperature:")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                    Text("\(String(format: "%.1f", temperature))°C")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                }
+                
+                // Conditions
+                HStack {
+                    Text("Conditions:")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                    Text("Sunny with some wind")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                }
+                
+                Divider()
+                
+                // About city
+                Text("About \(city.name)")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                   
+                
+                Text(city.description)
+                    .font(.body)
+                   
+            }
+            .padding()
+            .navigationTitle(city.name)
+            .navigationBarTitleDisplayMode(.inline)
+            .background(.thinMaterial, in: RoundedRectangle(cornerRadius: 8))
+        }
+        
+    }
+}
+   
 #Preview {
     WeatherView(city: cities[0])
 }
+
+
