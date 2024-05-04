55cd04b7e60f708417ab3fa4b4d8e3d67c8d99ec
himoji <ma.lyaka@bk.ru>
2024-05-04 20:11:24 +0500

    RC1

---
+++ TestApp/Views/WeatherDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherDetailsView.swift
+//  TestApp
+//
+//  Created by Lyaka on 04.05.2024.
+//
+
+import SwiftUI
+
+struct WeatherDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherDetailsView()
+}
